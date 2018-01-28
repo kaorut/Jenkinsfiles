622c59fc11650726d5d1599706da30a01d2fd6fb
kaoru <kaorut@mc.pikara.ne.jp>
2018-01-29 01:26:23 +0900

    Upgrade the Boost version to 1.66.0

--- tetengo2/tetengo2_config.h
+++ tetengo2/tetengo2_config.h
@@ -4,9 +4,9 @@
     Copy this file to the solution directory with the name "tetengo2_config.h"
     and edit it to build Tetengo2.
 
-    Copyright (C) 2007-2014 kaoru
+    Copyright (C) 2007-2018 kaoru
 
-    $Id: 64a093b21442e3081c9f92abf7486c264f8164ff $
+    $Id: c011c836bc72cf15cd2857a9eda0085570cd5453 $
 */
 
 #if !defined(TETENGO2CONFIG_H)
@@ -14,13 +14,13 @@
 
 
 // Set the Boost version.
-#define BOOST_VERSION "1_65_1"
+#define BOOST_VERSION "1_66"
 
 // Set the path where the 32-bit (Win32) version of Boost DLLs exist.
-#define BOOST_LIB_Win32 "C:\\data\\boost\\boost_1_65_1\\stage\\Win32\\lib"
+#define BOOST_LIB_Win32 "C:\\data\\boost\\boost_1_66_0\\stage\\Win32\\lib"
 
 // Set the path where the 64-bit (x64) version of Boost DLLs exist.
-#define BOOST_LIB_x64 "C:\\data\\boost\\boost_1_65_1\\stage\\x64\\lib"
+#define BOOST_LIB_x64 "C:\\data\\boost\\boost_1_66_0\\stage\\x64\\lib"
 
 // Set to 1 to run test programs for a Win32 (x86) platform.
 #define RUN_TEST_FOR_Win32 1
