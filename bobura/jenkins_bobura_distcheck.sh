a2cbc74031bbce0d7e39b97c221019655a9727c0
kaoru <kaorut@mc.pikara.ne.jp>
2018-03-03 06:50:48 +0900

    Specify clang-format at configure

--- bobura/jenkins_bobura_distcheck.sh
+++ bobura/jenkins_bobura_distcheck.sh
@@ -3,6 +3,7 @@
 SOURCE_TOP=$1
 
 export DISTCHECK_CONFIGURE_FLAGS=" \
+  --with-clang-format=clang-format-5.0 \
   --with-tetengo2-includedir=$SOURCE_TOP/lib/tetengo2/tetengo2/include \
   --with-tetengo2-libdir=$SOURCE_TOP/lib/tetengo2/tetengo2/src \
   --with-tetengo2-detail-includedir=$SOURCE_TOP/lib/tetengo2/tetengo2_detail/include \
